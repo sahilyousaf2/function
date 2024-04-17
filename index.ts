cb68582c88a2acca87c64392e6d885b1496185a6
Sahil Yousaf <160120573+sahilyousaf2@users.noreply.github.com>
2024-04-17 23:20:52 +0500

    first

---
+++ index.ts
@@ -0,0 +1,45 @@
+// Function 
+
+
+//addition function
+
+function addtion(num:number,num1:number){  // Declear With Paramiter 
+
+    console.log(`Addtion Result Is : ${num+num1}`); // Function Body 
+
+}
+
+addtion(16,10) // Function Call 
+
+
+
+//subtraction function
+
+function subtraction(num:number,num1:number){
+
+    console.log(`subtraction Result Is : ${num-num1}`);
+
+}
+subtraction(16,10)
+
+
+//multiplication function
+
+function multiplication(num:number,num1:number){
+
+    console.log(`Multiplication Result Is : ${num*num1}`);
+
+}
+multiplication(16,10)
+
+
+
+//division function
+
+function divsion(num:number,num1:number){
+
+    console.log(`Divsion Result Is : ${num/num1}`);
+
+}
+
+divsion(16,2)
